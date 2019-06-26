6b9ad475c84deb9db58b1af3335d7b5965cb33c7
VladyslavStarina <46134124+VladyslavStarina@users.noreply.github.com>
2019-06-26 12:38:16 +0300

    Create test.hpp

---
+++ test.hpp
@@ -0,0 +1,16 @@
+#ifndef _TEST_HPP_
+#define _TEST_HPP_
+
+void Test1(IntegerQueue & _queue);
+void Test2(IntegerQueue & _queue);
+void Test3(IntegerQueue & _queue);
+void Test4(IntegerQueue & _queue);
+void Test5(IntegerQueue & _queue);
+void Test6(IntegerQueue & _queue);
+void Test7(IntegerQueue & _queue);
+void Test8(IntegerQueue & _queue);
+void Test9(IntegerQueue & _queue);
+
+
+#endif 
+
