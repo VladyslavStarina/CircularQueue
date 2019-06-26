e5a8a497eed3d381c2dbaf72ea225f9367790e99
VladyslavStarina <46134124+VladyslavStarina@users.noreply.github.com>
2019-06-26 12:39:40 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include "integer_queue.hpp"
+#include "test.hpp"
+
+int main()
+{
+	IntegerQueue queue;
+
+	Test1(queue);
+	Test2(queue);
+	Test3(queue);
+	Test4(queue);
+	Test5(queue);
+	Test6(queue);
+	Test7(queue);
+	Test8(queue);
+	Test9(queue);
+
+	return 0;
+}
